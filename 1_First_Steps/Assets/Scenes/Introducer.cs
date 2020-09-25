b212d2889f642bc557bc698829de722724c52ddc
Soe Wei Yan Phyo <soew@student.unimelb.edu.au>
2020-09-25 16:48:12 +1000

    Getting started with unity.

---
+++ 1_First_Steps/Assets/Scenes/Introducer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour {
+
+    private void Start () {
+        print("Hello Soe Wei Yan");
+    }
+
+    private void Update () {
+        
+    }
+}
