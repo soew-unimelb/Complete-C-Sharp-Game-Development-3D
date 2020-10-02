b8cb1599a6bf3f71552188bbe47a7f03e67b527a
Soe Wei Yan Phyo <aldrik.swyp@gmail.com>
2020-10-02 14:02:34 +1000

    3_PB_Rocket.cs: Input logic added.

--- 3_Project_Boost/Assets/Scripts/Rocket.cs
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Rocket : MonoBehaviour {
+
+    private void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        ProcessInput();    
+    }
+
+    private void ProcessInput() {
+
+        // Can thrust while rotating
+        if (Input.GetKey(KeyCode.Space)) {
+            print("Thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            print("Rotating right");
+        }
     }
 }
