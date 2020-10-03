d19243631a8f59a9f25e6ee4fe8decfa077d28cb
Soe Wei Yan Phyo <aldrik.swyp@gmail.com>
2020-10-03 18:07:58 +1000

    3_PB_Oscillator.cs: Testing range() and transform.position

---
+++ 3_Project_Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] private Vector3 movementVector;
+    // TODO remove from inspector later
+    [SerializeField] [Range(0, 1)] private float movementFactor; // 0 for not moved, 1 for fully moved.
+
+    private Vector3 startingPos;
+
+    private void Start() {
+
+        startingPos = transform.position;
+    }
+
+    private void Update() {
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
+
