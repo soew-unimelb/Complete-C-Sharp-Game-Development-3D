d1463a83531144b917f4ca6434d2b01c76e2830c
Soe Wei Yan Phyo <aldrik.swyp@gmail.com>
2020-10-03 18:46:34 +1000

    3_PB_Oscillator.cs:  Sin function to osicillate (added)

--- 3_Project_Boost/Assets/Scripts/Oscillator.cs
+++ 3_Project_Boost/Assets/Scripts/Oscillator.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] private Vector3 movementVector;
+    [SerializeField] private Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] private float period = 2f;
+
     // TODO remove from inspector later
     [SerializeField] [Range(0, 1)] private float movementFactor; // 0 for not moved, 1 for fully moved.
 
@@ -18,6 +20,13 @@ public class Oscillator : MonoBehaviour {
 
     private void Update() {
 
+        // TODO protect against period = 0
+        float cycles = Time.time / period; // Grows continually from 0
+
+        const float tau = 2f * Mathf.PI;
+        float rawSineWave = Mathf.Sin(cycles * tau); // from -1 to +1
+
+        movementFactor = rawSineWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
